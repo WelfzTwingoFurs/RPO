8e027d6a8d08eb4892ea70a900c2ccc76e861253
smellyweggy <60985201+WelfzTwingoFurs@users.noreply.github.com>
2021-11-12 16:28:59 -0300

    main, 12/11

--- AnimationPlayer.gd
+++ AnimationPlayer.gd
@@ -16,7 +16,9 @@ func _process(delta):
 	$Sprite4.frame = $Sprite.frame + 10
 	$Sprite5.frame = $Sprite.frame + 9
 	$Sprite6.frame = $Sprite.frame + 39
-
+	$Sprite8.frame = $Sprite.frame + 9
+	$Sprite9.frame = $Sprite.frame + 19
+	$Sprite10.frame = $Sprite.frame + 29
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
